46b65f1934127f11bfbfe5db92b9f30c45269b14
ullink <ullink@7-rchanhohang2.ullink.lan>
2014-05-08 15:00:13 +0800

    groovy implementation

--- groovy/src/SquareCountry.groovy
+++ groovy/src/SquareCountry.groovy
@@ -3,11 +3,83 @@
  */
 class SquareCountry {
 
+    final int MAX_SIZE = 600000;
+    int[] minCertStore = new int[MAX_SIZE];
+    List<Integer> [] minCertCombinationStore = new List<Integer>[MAX_SIZE];
+
+    public SquareCountry()
+    {
+        for (int i=1;i<=MAX_SIZE;i++)
+        {
+            lookupCertificate(i);
+        }
+    }
+
+    public void dumpArray()
+    {
+        for (int i=1;i<=MAX_SIZE;i++)
+        {
+            System.out.println(i + " with min Cert: " + minCertStore[i-1] + "|" + minCertCombinationStore[i-1]);
+
+            if (minCertStore[i-1] > 4)
+            {
+                break;
+            }
+        }
+
+    }
     public int lookupCertificate(int quadrics)
     {
         //TODO
 
-        return 0;
+        if (minCertStore[quadrics-1] != 0)
+        {
+            return minCertStore[quadrics-1];
+        }
+
+        int minCert = quadrics + 1;
+        int minCertCombinationQuadrics = -1;
+        int minSquareRt = -1;
+
+        try
+        {
+            int squareRt = Math.sqrt(quadrics);
+            for (int i=squareRt; i> 0;i--)
+            {
+                int square = i*i;
+                int nextQuadrics = quadrics - square;
+
+                if (nextQuadrics == 0)
+                {
+                    minCert = 1;
+                    minSquareRt = i;
+                    break;
+                }
+                else
+                {
+                    int cert = lookupCertificate(nextQuadrics) + 1;
+                    if (minCert > cert)
+                    {
+                        minSquareRt = i;
+                        minCertCombinationQuadrics = nextQuadrics;
+                        minCert = cert;
+                    }
+                }
+            }
+            return minCert;
+
+        }
+        finally
+        {
+            minCertCombinationStore[quadrics-1] = new ArrayList<>();
+            minCertCombinationStore[quadrics-1].add(minSquareRt);
+            if (minCertCombinationQuadrics != -1)
+            {
+                minCertCombinationStore[quadrics-1].addAll(minCertCombinationStore[minCertCombinationQuadrics-1]);
+            }
+
+            minCertStore[quadrics-1] = minCert;
+        }
     }
 
 }
--- groovy/test/TestSquareCountry.groovy
+++ groovy/test/TestSquareCountry.groovy
@@ -6,7 +6,21 @@ class TestSquareCountry extends GroovyTestCase {
     void test()
     {
         def squareCountry = new SquareCountry();
+        Assert.assertEquals(4, squareCountry.lookupCertificate(7));
+        Assert.assertEquals(3, squareCountry.lookupCertificate(11));
+        Assert.assertEquals(1, squareCountry.lookupCertificate(256));
+
         Assert.assertEquals(3, squareCountry.lookupCertificate(344));
+
+        Assert.assertEquals(2, squareCountry.lookupCertificate(200));
+
+        Assert.assertEquals(2, squareCountry.lookupCertificate(200));
+
+        Assert.assertEquals(1, squareCountry.lookupCertificate((int)Math.pow(200,2)));
+
+        Assert.assertEquals(1, squareCountry.lookupCertificate((int)Math.pow(230,2)));
+
+        squareCountry.dumpArray();
     }
 
 }
